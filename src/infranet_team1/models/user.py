4bd8949303b462a98f82432e3ecd836a87904653
Seohyun Kim <skim291@gmu.edu>
2025-06-20 12:45:26 +0900

    some refactor

--- src/infranet_team1/models/user.py
+++ src/infranet_team1/models/user.py
@@ -1,6 +1,8 @@
+from bson import ObjectId
+
 class User:
     def __init__(self, user_data):
-        self.id = str(user_data["_id"])
+        self.id = ObjectId(user_data["_id"])
         self.name = user_data["name"]
         self.email = user_data["email"]
         self.role = user_data.get("role", "user")
